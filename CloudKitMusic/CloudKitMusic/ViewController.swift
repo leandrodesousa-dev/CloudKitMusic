35ef31e93d43c310c73cb8b8b5b69a606c12433f
Leandro <leandrooodesousa@gmail.com>
2019-10-08 14:10:55 -0400

    Insert project xcode

---
+++ CloudKitMusic/CloudKitMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudKitMusic
+//
+//  Created by Leandro de Sousa Silva on 08/10/19.
+//  Copyright © 2019 AcademyMistic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
